82f4952ecf90c5e79504cf4f9aea355b9739bde9
Ahmad Raza <ahmdrzalifa@gmail.com>
2021-02-18 20:57:46 +0500

    Updated middleware function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jwt-redis-sessions
 Generate JWT Tokens and manage sessions
+
+NOTE: Docs and examples will be added soon
--- auth.middleware.js
+++ auth.middleware.js
@@ -1,33 +1,45 @@
 const { verifyToken } = require('./jwt.service')
 
 exports.auth = async (req, res, next) => {
-    if (req.jrs && req.jrs.secret) {
-        if (typeof secret !== 'string') {
-            res.status(401).json({
-                status: 'INTERNAL_ERROR',
-                message: 'Secret should be a string'
-            })
-        }
+  let errorMessage = null
 
-        if (secret === '') {
-            res.status(401).json({
-                status: 'INTERNAL_ERROR',
-                message: 'Secret cannot be empty'
-            })
-        }
-        try {
-            await verifyToken(req.jrs.secret, req.body.token)
-            next()
-        } catch (error) {
-            res.status(401).json({
-                status: 'UNAUTHORIZED',
-                message: error.message
-            })
-        }       
-    } else {
-        res.status(401).json({
-            status: 'INTERNAL_ERROR',
-            message: 'Secret Not Found'
-        })
-    }
+  if (!req.jrs || !req.jrs.secret) {
+    errorMessage = 'Secret is required'
+  } else if (req.jrs.secret === '') {
+    errorMessage = 'Secret cannot be empty'
+  } else if (typeof req.jrs.secret !== 'string') {
+    errorMessage = 'Secret should be a string'
+  } else if (!req.headers.authorization) {
+    errorMessage = 'Authorization header not found'
+  } else if (req.headers.authorization === '') {
+    errorMessage = 'Authorization header cannot be empty'
+  } else if (typeof req.headers.authorization !== 'string') {
+    errorMessage = 'Invalid authorization header structure'
+  }
+
+  if (errorMessage) {
+    res.status(401).json({
+      status: 'UNAUTHORIZED',
+      message: errorMessage,
+    })
+  }
+
+  let splitHeaderData = req.headers.authorization.split(' ')
+
+  if (splitHeaderData.length < 1) {
+    res.status(401).json({
+      status: 'UNAUTHORIZED',
+      message: 'Invalid authorization header structure',
+    })
+  }
+
+  try {
+    await verifyToken(req.jrs.secret, splitHeaderData[1])
+    next()
+  } catch (error) {
+    res.status(401).json({
+      status: 'UNAUTHORIZED',
+      message: error.message,
+    })
+  }
 }
