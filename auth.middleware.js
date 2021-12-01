a87e2d632af089ee563437326ba84b01ebd81574
Ahmad Raza <ahmdrzalifa@gmail.com>
2021-12-01 19:49:23 +0500

    improvemnets

--- auth.middleware.js
+++ auth.middleware.js
@@ -1,4 +1,4 @@
-const { verifyToken } = require('./jwt.service')
+const { verifyToken, refreshToken } = require('./jwt.service')
 
 exports.auth = async (req, res, next) => {
   let message = null
@@ -31,7 +31,50 @@ exports.auth = async (req, res, next) => {
 
   try {
     await verifyToken(splitHeaderData[1])
-    next()
+    return next()
+  } catch (error) {
+    return res.status(401).json({
+      status: 'UNAUTHORIZED',
+      message: error.message,
+    })
+  }
+}
+
+exports.refreshToken = async (req, res, next) => {
+  let message = null
+
+  if (!process.env.JWT_SECRET) {
+    message = 'Secret is required'
+  } else if (typeof process.env.JWT_SECRET !== 'string') {
+    message = 'Secret should be a string'
+  } else if (!req.headers.authorization) {
+    message = 'Authorization header not found'
+  } else if (typeof req.headers.authorization !== 'string') {
+    message = 'Invalid authorization header structure'
+  }
+
+  if (message) {
+    return res.status(401).json({
+      status: 'UNAUTHORIZED',
+      message: message,
+    })
+  }
+
+  let splitHeaderData = req.headers.authorization.split(' ')
+
+  if (splitHeaderData.length < 1) {
+    return res.status(401).json({
+      status: 'UNAUTHORIZED',
+      message: 'Invalid authorization header structure',
+    })
+  }
+
+  try {
+    const token = await refreshToken(splitHeaderData[1])
+    return res.status(200).json({
+      status: 'SUCCESS',
+      token,
+    })
   } catch (error) {
     return res.status(401).json({
       status: 'UNAUTHORIZED',
--- jwt.service.js
+++ jwt.service.js
@@ -2,82 +2,61 @@ var jwt = require('jsonwebtoken')
 
 const { redisClient } = require('./redis.config')
 
-exports.generateToken = (secret, data = {}, dataToStoreInToken = {}) => {
-  return new Promise((resolve, reject) => {
-    if (!secret) {
-      reject({ message: 'Secret is required' })
-    }
-
-    if (typeof secret !== 'string') {
-      reject({ message: 'Secret should be a string' })
-    }
-
-    const msBeforeTokenGenerated = new Date().getTime()
-    const token = jwt.sign(dataToStoreInToken, secret)
-
-    const msAfterTokenGenerated = new Date().getTime()
-    let timeElapsed = msAfterTokenGenerated - msBeforeTokenGenerated
-    timeElapsed = ((timeElapsed % 60000) / 1000).toFixed(0) //convert ms to sec
-    let expTime = 60 * 60 * 24
-    expTime = expTime - timeElapsed
-
-    const _data = { ...data }
-    _data.lastRefresh = Math.floor(new Date().getTime() / 1000)
-
-    redisClient.set(token, JSON.stringify(_data), 'EX', expTime, (err) => {
-      if (err) {
-        reject({ message: 'Redis encountered an error', details: err })
-      }
-      resolve(token)
-    })
-  })
-}
-
-exports.verifyToken = (token) => {
+exports.generateToken = (data = {}) => {
   return new Promise((resolve, reject) => {
     if (!process.env.JWT_SECRET) {
       reject({ message: 'Secret is required' })
     }
 
+    if (typeof process.env.JWT_SECRET !== 'string') {
+      reject({ message: 'Secret should be a string' })
+    }
+
+    const token = jwt.sign({}, process.env.JWT_SECRET)
+    const expTime = 60 * 60 * 24
+
+    redisClient.set(
+      token,
+      JSON.stringify({ ...data }),
+      'EX',
+      expTime,
+      (err) => {
+        if (err) {
+          reject({ message: 'Redis encountered an error', details: err })
+        }
+        resolve(token)
+      }
+    )
+  })
+}
+
+exports.verifyToken = (token) => {
+  return new Promise((resolve, reject) => {
     jwt.verify(token, process.env.JWT_SECRET)
 
-    redisClient.get(token, function (err, reply) {
+    redisClient.get(token, (err, reply) => {
       if (err) reject({ message: 'Token not found' })
       if (reply) {
-        const tokenData = JSON.parse(reply)
-        let lastRefresh = tokenData.lastRefresh
-        var currentTime = Math.floor(new Date().getTime() / 1000)
-        const diff = lastRefresh - currentTime
-
-        if (diff < 0) {
-          reject({ message: 'Authorization token expired' })
-        }
-
-        if (diff < 3600) {
-          const newCurrentTime = Math.floor(new Date().getTime() / 1000)
-          expiryTime = newCurrentTime + 86400
-        }
-
-        const updatedData = {
-          ...tokenData,
-          lastRefresh: Math.floor(new Date().getTime() / 1000),
-          expiryTime: expiryTime,
-        }
-
-        redisClient.set(
-          token,
-          JSON.stringify(updatedData),
-          'EX',
-          expiryTime,
-          (err) => {
-            if (err) {
-              reject({ message: 'Redis encountered an error', details: err })
-            }
-            resolve('success')
-          }
-        )
+        resolve('success')
       } else {
-        reject({ message: 'Authorization token not found' })
+        reject({ message: 'Token not found' })
+      }
+    })
+  })
+}
+
+exports.refreshToken = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET)
+    redisClient.get(token, async (err, reply) => {
+      if (err) reject({ message: 'Token not found' })
+      if (reply) {
+        const data = JSON.parse(reply)
+        redisClient.del(token)
+        const newToken = await this.generateToken(data)
+        resolve(newToken)
+      } else {
+        reject({ message: 'Token not found' })
       }
     })
   })
